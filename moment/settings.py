aa52aad032f5d29074d7f13516800e8e338fcbb3
Ana Niko <ananikolayenia@gmail.com>
2024-07-04 16:00:20 +0000

    feat: add profile to nav

--- moment/settings.py
+++ moment/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "8000-ananikobyte-moment-afvpc7dloam.ws-us114.gitpod.io",
+    "8000-ananikobyte-moment-d3p4f1abj0r.ws-us115.gitpod.io",
     ".herokuapp.com",
 ]
 
@@ -163,7 +163,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
